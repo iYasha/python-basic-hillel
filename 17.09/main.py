042326d6ed37e928c4274dd4d5e22c065ede32e1
iYasha <33287747+iYasha@users.noreply.github.com>
2022-09-24 11:33:41 +0300

    17.09

--- 17.09/main.py
+++ 17.09/main.py
@@ -80,21 +80,103 @@
 
 """
 1) Пользователя вводит два числа A и B, нужно вывести сумму чисел в диапазоне от A до B.
+"""
+
+# A = int(input('Enter number #1: '))
+# B = int(input('Enter number #2: '))
+# summ = 0
+#
+# # A = 10
+# # B = 4
+# if A > B:
+#     print('error!')
+#     exit(4)
+# if A > B:
+#     A, B = B, A
+#
+#
+# while A < B:
+#     summ += A
+#     A += 1
+
+
+
+
+"""
 2) Пользователь вводит два числа A и B, нужно вывести сумму всех четных чисел в диапазоне от A до B.
+"""
+
+# A = int(input('Enter number #1: '))
+# B = int(input('Enter number #2: '))
+# summ = 0
+#
+# while A < B:
+#     if A % 2 == 0:
+#         summ += A
+#         print(A)
+#     A += 1
+#
+# print(summ)
+
+"""
 3) Пользователь вводит два числа A и B, нужно вывести прямоугольник со сторонами A и B с помощью символа '*' используя цикл.
 В коде можно использовать символ '*' только один раз.
+"""
+
+# A = int(input('Enter number #1: '))
+# B = int(input('Enter number #2: '))
+# main_symbol = '*'
+# temp = 0
+
+"""
+A = 4
+B = 5
+
+****
+****
+****
+****
+****
+
+main_symbol * A
+"""
+
+# print((('*' * A) + '\n') * B)
+
+# while temp < B:
+#     i = 0
+#     while i < A:
+#         print(main_symbol, end='')
+#         i += 1
+#     print()
+#     temp += 1
+#
+#
+# while temp < B:
+#     print(main_symbol * A)
+#     temp += 1
+
+
+
+"""
 4) Запросить у пользователя число N, вывести треугольник шириной N используя символ '*'.
 Input:
 N = 6
 Output:
-******
-*****
-****
-***
-**
-*
+6 ******
+5 *****
+4 ****
+3 ***
+2 **
+1 *
 """
 
+N = int(input('Enter number: '))
+main_symbol = '*'
+
+while N > 0:
+    print(main_symbol * N)
+    N -= 1
 
 
 
